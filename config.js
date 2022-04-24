b1d174b28e429692cc57493e72288c430bde89f9
fakeaboo <104327528+fakeaboo@users.noreply.github.com>
2022-04-24 18:44:10 -0400

    Update config.js

--- config.js
+++ config.js
@@ -24,7 +24,7 @@ const ICONS = [
 const LIST_ITEMS = [
   {
     name: "music",
-    link: "https://music.apple.com/ph/browse/"
+    link: "https://music.apple.com/ph/listen-now"
   },
   {
     name: "netflix",
